d9c379c91c85b691a2a87f85524bc115639d0451
Vidyasekhar Potluri <potluri@oakland.edu>
2022-03-26 16:26:30 -0700

    Added LIDAR to Audibot UDRF model

--- src/AudibotCruise.cpp
+++ src/AudibotCruise.cpp
@@ -16,10 +16,16 @@ double a2_path_linx = 0;
 
 double dist_a1_a2 = 0;
 
-const double target_dist = 20;
+const double target_dist = 22;
+
+const int laser_readings = 360;
 
 
 geometry_msgs::Twist cmd_vel_a1, cmd_vel_a2;
+
+double laser_ranges[laser_readings];
+double laser_angles[laser_readings];
+
 
 // Namespace matches ROS package name
 namespace audibot_final_project {
@@ -27,10 +33,11 @@ namespace audibot_final_project {
   // Constructor with global and private node handle arguments
   final_project::final_project(ros::NodeHandle n, ros::NodeHandle pn)
   {
-    gps_a1_sub =  n.subscribe("/a1/gps/fix", 1, &final_project::recvFixa1, this);
-    gps_a2_sub =  n.subscribe("/a2/gps/fix", 1, &final_project::recvFixa2, this);
-    vel_path_a1 =  n.subscribe("/a1/path/cmd_vel", 1, &final_project::recvPathVela1, this);
-    vel_path_a2 =  n.subscribe("/a2/path/cmd_vel", 1, &final_project::recvPathVela2, this);
+    gps_a1_sub =  n.subscribe("/a1/gps/fix", 1, &final_project::recvFix_a1, this);
+    gps_a2_sub =  n.subscribe("/a2/gps/fix", 1, &final_project::recvFix_a2, this);
+    laser_a1_aub = n.subscribe("/a1/front_laser/scan", 1, &final_project::recvLaserScan_a1, this);
+    vel_path_a1 =  n.subscribe("/a1/path/cmd_vel", 1, &final_project::recvPathVel_a1, this);
+    vel_path_a2 =  n.subscribe("/a2/path/cmd_vel", 1, &final_project::recvPathVel_a2, this);
 
     vel_a1_pub = n.advertise<geometry_msgs::Twist>("/a1/cmd_vel", 1); 
     vel_a2_pub = n.advertise<geometry_msgs::Twist>("/a2/cmd_vel", 1); 
@@ -38,7 +45,7 @@ namespace audibot_final_project {
     level1_timer = n.createTimer(ros::Duration(0.01), &final_project::level1TimerCallback, this);
   }
   
-  void final_project::recvFixa1(const sensor_msgs::NavSatFixConstPtr& msg)
+  void final_project::recvFix_a1(const sensor_msgs::NavSatFixConstPtr& msg)
   {
     UTMCoords current_coords_a1(*msg);
 
@@ -49,7 +56,7 @@ namespace audibot_final_project {
     // ROS_INFO("a1 UTM Coodinates: (%f, %f, %f)", a1_x, a1_y, a1_z); 
   }
 
-  void final_project::recvFixa2(const sensor_msgs::NavSatFixConstPtr& msg)
+  void final_project::recvFix_a2(const sensor_msgs::NavSatFixConstPtr& msg)
   {
     UTMCoords current_coords_a2(*msg);
 
@@ -60,16 +67,25 @@ namespace audibot_final_project {
     // ROS_INFO("a2 UTM Coodinates: (%f, %f, %f)", a2_x, a2_y, a2_z);  
   }
 
-  void final_project::recvPathVela1(const geometry_msgs::TwistConstPtr& msg)
+  void final_project::recvPathVel_a1(const geometry_msgs::TwistConstPtr& msg)
   {
     a1_path_linx = msg->linear.x;
     a1_path_angz = msg->angular.z;
   }
 
-  void final_project::recvPathVela2(const geometry_msgs::TwistConstPtr& msg)
+  void final_project::recvPathVel_a2(const geometry_msgs::TwistConstPtr& msg)
   {
     a2_path_linx = msg->linear.x;
     a2_path_angz = msg->angular.z;
+  }
+
+  void final_project::recvLaserScan_a1(const sensor_msgs::LaserScanConstPtr& msg)
+  {
+    for(int i=0; i<laser_readings; i++){
+      laser_ranges[i] = msg->ranges[i];
+      laser_angles[i] = msg->angle_min + (i * msg->angle_increment);
+    }
+    // ROS_INFO("Laser scan at -45, 0, 45: (%f, %f, %f)", laser_ranges[0], laser_ranges[180], laser_ranges[359]);
   }
 
   void final_project::level1TimerCallback(const ros::TimerEvent& event)
@@ -84,7 +100,7 @@ namespace audibot_final_project {
     ROS_INFO("Distance: %f", dist_a1_a2);
 
     if (dist_a1_a2 < target_dist){
-      cmd_vel_a1.linear.x = 18;
+      cmd_vel_a1.linear.x = 19;
       cmd_vel_a1.angular.z = a1_path_angz;
     }
     else{
--- src/AudibotCruise.h
+++ src/AudibotCruise.h
@@ -1,11 +1,13 @@
 // ROS header
 #include <ros/ros.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float64.h>
 #include <ugv_course_libs/gps_conv.h>
 #include <tf/tf.h> 
 #include <tf/transform_broadcaster.h> 
 #include <geometry_msgs/Twist.h> 
+#include <laser_geometry/laser_geometry.h>
 #include <math.h>
 
 // Namespace matches ROS package name
@@ -18,14 +20,17 @@ class final_project
 
   private:
   void level1TimerCallback(const ros::TimerEvent& event);
-  void recvFixa1(const sensor_msgs::NavSatFixConstPtr& msg);
-  void recvFixa2(const sensor_msgs::NavSatFixConstPtr& msg);
-  void recvPathVela1(const geometry_msgs::TwistConstPtr& msg);
-  void recvPathVela2(const geometry_msgs::TwistConstPtr& msg);
+  void recvFix_a1(const sensor_msgs::NavSatFixConstPtr& msg);
+  void recvFix_a2(const sensor_msgs::NavSatFixConstPtr& msg);
+  void recvPathVel_a1(const geometry_msgs::TwistConstPtr& msg);
+  void recvPathVel_a2(const geometry_msgs::TwistConstPtr& msg);
+  void recvLaserScan_a1(const sensor_msgs::LaserScanConstPtr& msg);
   ros::Subscriber gps_a1_sub;
   ros::Subscriber gps_a2_sub;
   ros::Subscriber vel_path_a1;
   ros::Subscriber vel_path_a2;
+  ros::Subscriber laser_a1_aub;
+  ros::Subscriber laser_a2_sub;
   ros::Publisher vel_a1_pub;
   ros::Publisher vel_a2_pub;
   ros::Timer level1_timer;
